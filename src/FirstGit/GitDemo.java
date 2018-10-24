7e6d951b4a524a3485c3fea4f63969067d5b1055
dinadas <Dkg031293@gmail.com>
2018-10-25 04:39:35 +0530

    Performed addition operation

---
+++ src/FirstGit/GitDemo.java
@@ -0,0 +1,12 @@
+package FirstGit;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		int a=10, b=20,c;
+		c=a+b;
+		System.out.println("Value of a is :" +a);
+		System.out.println("Value of b is :" +b);
+		System.out.println("Addition of a and b is :" +c);
+	}
+
+}
